c1cebb30849ebd89e9292be602fd0086dd35342d
Marcio Cruz de Almeida <marciocadev@gmail.com>
2026-06-20 22:45:06 -0300

    adding tag version

--- a-tf-stack/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "standard_topic_arn" {
-  description = "ARN of SNS topic standard-topic"
-  value       = aws_sns_topic.standard_topic.arn
-}
-
-output "fifo_topic_fifo_arn" {
-  description = "ARN of SNS topic fifo-topic.fifo"
-  value       = aws_sns_topic.fifo_topic_fifo.arn
-}
--- a-tf-stack/sns.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_sns_topic" "standard_topic" {
-  name = "standard-topic"
-}
-
-resource "aws_sns_topic" "fifo_topic_fifo" {
-  name = "fifo-topic.fifo"
-  fifo_topic = true
-}
